d0acd13b47ecfa54d1d68c1882d945bc682a4fe8
olzhas2102 <olzhas2102@yahoo.com>
2017-12-08 16:39:48 +0600

    Добавил отправку формы на сервер

--- js/backend.js
+++ js/backend.js
@@ -9,7 +9,7 @@
       xhr.responseType = 'json';
       xhr.addEventListener('load', function () {
         if (xhr.status === 200) {
-          console.log(xhr);
+          // console.log(xhr);
           onLoad(xhr.response);
         } else {
           onError(xhr.status + ': ' + xhr.statusText);
@@ -29,7 +29,7 @@
     },
 
     save: function (data, onLoad, onError) {
-      var URL = 'https://1510.dump.academy/keksobooking/data';
+      var URL = 'https://1510.dump.academy/keksobooking';
       var xhr = new XMLHttpRequest();
 
       xhr.responseType = 'json';
@@ -51,6 +51,23 @@
       xhr.timeout = 7000;
       xhr.open('POST', URL);
       xhr.send(data);
+    },
+
+    error: function (message) {
+      var errorBlock = document.createElement('div');
+      errorBlock.style.width = 50 + '%';
+      errorBlock.style.height = 120 + 'px';
+      errorBlock.style.position = 'fixed';
+      errorBlock.style.top = 50 + '%';
+      errorBlock.style.left = 50 + '%';
+      errorBlock.style.transform = 'translate(-50%, -50%)';
+      errorBlock.style.zIndex = 5;
+      errorBlock.style.display = 'flex';
+      errorBlock.style.alignItems = 'center';
+      errorBlock.style.justifyContent = 'center';
+      errorBlock.style.border = '3 px solid red';
+      errorBlock.textContent = message;
+      document.body.insertAdjacentElement('afterbegin', errorBlock);
     }
   };
 })();
